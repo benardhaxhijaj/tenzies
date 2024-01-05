20094df0aa53e238ba29fcf5b772d0160eb5a556
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2024-01-05 20:45:15 +0100

    feat(App): Implement dice rolling functionality
    This commit introduces the ability to roll dice in the application. The App component now maintains a state representing the values of the dice, and a button has been added to trigger a new roll. Each die is represented by a Die component, which displays the value of the die.

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,22 +1,30 @@
-function App() {
+import React from "react";
+import Die from "./Die";
+
+export default function App() {
+  const allNewDice = () => {
+    let diceArray = [];
+    for (let i = 0; i < 10; i++) {
+      diceArray.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return diceArray;
+  };
+  const [dice, setDice] = React.useState(allNewDice());
+
+  const diceElements = dice.map((die, index) => (
+    <Die value={die} key={index} />
+  ));
+
+  const rollBtn = () => {
+    setDice(allNewDice());
+  };
+
   return (
     <main>
-      <div className="row">
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-      </div>
-      <div className="row">
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-        <div className="dice-com">1</div>
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="rollingBtn" onClick={rollBtn}>
+        Roll Dice
+      </button>
     </main>
   );
 }
-
-export default App;
