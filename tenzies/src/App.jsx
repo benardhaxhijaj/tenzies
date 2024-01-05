7bd9657cc1c60d2424565599f16ebe06d9933a26
Benard Haxhijaj <benardhaxhijaj@gmail.com>
2024-01-06 00:24:10 +0100

    This commit adds the ability to hold a dice when clicked. The holdDice function is now called with the id of the clicked die, allowing us to track which dice are being held.
    Changes made:
    - Added onClick handler to each Die component in App.jsx
    - Passed holdDice function as onClick prop to each Die component
    - Updated Die component to accept onClick prop and assign it to div's onClick handler

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
   const allNewDice = () => {
     let diceArray = [];
     for (let i = 0; i < 10; i++) {
-      diceArray.push(Math.floor(Math.random() * 6) + 1);
+      diceArray.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return diceArray;
   };
+
   const [dice, setDice] = React.useState(allNewDice());
 
-  const diceElements = dice.map((die, index) => (
-    <Die value={die} key={index} />
+  const diceElements = dice.map((die) => (
+    <Die
+      value={die.value}
+      isHeld={die.isHeld}
+      key={die.id}
+      handleClick={() => handleClick(die.id)}
+    />
   ));
 
   const rollBtn = () => {
     setDice(allNewDice());
+  };
+
+  const handleClick = (id) => {
+    console.log(id);
   };
 
   return (
--- tenzies/src/Die.jsx
+++ tenzies/src/Die.jsx
@@ -1,7 +1,8 @@
-const Die = (props) => {
+const Die = ({ value, isHeld, handleClick }) => {
+  const dieClass = isHeld ? "green-face" : "";
   return (
-    <div className="die-face">
-      <h2 className="die-num">{props.value}</h2>
+    <div className={`die-face ${dieClass}`} onClick={handleClick}>
+      <h2 className="die-num">{value}</h2>
     </div>
   );
 };
